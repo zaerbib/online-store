b1919c738a7657aaeedb0c7361de2d1f74cacc56
zaerbib <ilunga.joel@yahoo.fr>
2024-01-13 12:27:28 +0100

    update page and custom it

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/")
+  @Render('index')
+  index() {
+    let viewData = [];
+    viewData['title'] = 'Home Page - Online Store';
+    return {
+      viewData: viewData
+    };
+  }
+
+  @Get("/about")
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['title'] = 'About us - Online Store';
+    viewData['subtitle'] = "About us";
+    viewData['description'] = "This is an about page ...";
+    viewData['author'] = 'Developed by: Your Name';
+
+    return {
+        viewData: viewData
+    };
   }
 }
