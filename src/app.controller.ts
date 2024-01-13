c1ce76bf0915d4d99cbaf31bd85ba01f65712c11
zaerbib <ilunga.joel@yahoo.fr>
2024-01-13 21:07:47 +0100

    integrate mongo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,29 +3,29 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
-  @Get("/")
+  @Get('/')
   @Render('index')
   index() {
-    let viewData = [];
+    const viewData = [];
     viewData['title'] = 'Home Page - Online Store';
     return {
-      viewData: viewData
+      viewData: viewData,
     };
   }
 
-  @Get("/about")
+  @Get('/about')
   @Render('about')
   about() {
     const viewData = [];
     viewData['title'] = 'About us - Online Store';
-    viewData['subtitle'] = "About us";
-    viewData['description'] = "This is an about page ...";
+    viewData['subtitle'] = 'About us';
+    viewData['description'] = 'This is an about page ...';
     viewData['author'] = 'Developed by: Your Name';
 
     return {
-        viewData: viewData
+      viewData: viewData,
     };
   }
 }
