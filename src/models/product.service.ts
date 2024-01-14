3d2a600fca18960d4e998cf1d6244ba3b00a9915
zaerbib <ilunga.joel@yahoo.fr>
2024-01-14 15:51:41 +0100

    request to mongo database

---
+++ src/models/product.service.ts
@@ -0,0 +1,18 @@
+import { Model } from "mongoose";
+import { Product } from "./product.schema";
+import { InjectModel } from "@nestjs/mongoose";
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable()
+export class ProductService {
+    constructor(@InjectModel(Product.name) private productModel: Model<Product>) {}
+
+    async findAll(): Promise<Product[]> {
+        return this.productModel.find().exec();
+    }
+
+    async findOne(id: string): Promise<Product> {
+        return this.productModel.findOne({id: id}).exec();
+    }
+}
